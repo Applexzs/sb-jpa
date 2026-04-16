ad8819aab5a1cc4447fac5af47009c54fad1c196
Applexzs <reverseapplemw2@gmail.com>
2026-04-16 10:06:42 -0600

    Added some methods on Repository & we test them on spring main class

--- src/main/java/com/applexzs/springboot/jpa/SpringbootJpaApplication.java
+++ src/main/java/com/applexzs/springboot/jpa/SpringbootJpaApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 public class SpringbootJpaApplication implements CommandLineRunner {
@@ -21,8 +22,31 @@ public class SpringbootJpaApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        List<Person> persons = (List<Person>) repository.findAll();
-
-        persons.stream().forEach(person -> System.out.println(person));
+        findOne();
     }
+
+    public void findOne() {
+//        Person person = null;
+//        Optional<Person> optionalPerson = repository.findById(8L);
+//        if(optionalPerson.isPresent()) {
+//            person = optionalPerson.get();
+//        }
+//        System.out.println(person);
+        repository.findById(1L).ifPresent(person -> System.out.println(person));
+    }
+
+    public void list() {
+        //List<Person> persons = (List<Person>) repository.findAll();
+        //List<Person> persons = (List<Person>) repository.buscarPorProgrammingLanguage("Java", "Andres");
+
+        List<Person> persons = (List<Person>) repository.findByProgrammingLanguageAndName("Java", "Andres");
+        persons.stream().forEach(person -> System.out.println(person));
+
+        List<Object[]> personValues = repository.obtenerPersonData();
+        personValues.stream().forEach(person -> {
+            System.out.println(person[0] + " es experto en " + person[1]);
+            System.out.println(person);
+        });
+    }
+
 }
--- src/main/java/com/applexzs/springboot/jpa/repositories/IPersonRepository.java
+++ src/main/java/com/applexzs/springboot/jpa/repositories/IPersonRepository.java
@@ -1,7 +1,24 @@
 package com.applexzs.springboot.jpa.repositories;
 
 import com.applexzs.springboot.jpa.entities.Person;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface IPersonRepository extends CrudRepository<Person, Long> {
+
+    @Query("select p from Person p where p.programmingLanguage=?1 and p.name=?2")
+    List<Person> buscarPorProgrammingLanguage(String programmingLanguage, String name);
+
+    List<Person> findByProgrammingLanguageAndName(String programmingLanguage, String name);
+
+    @Query("select p.name, p.programmingLanguage from Person p")
+    List<Object[]>  obtenerPersonData();
+
+    @Query("select p.name, p.programmingLanguage from Person p where p.name=?1")
+    List<Object[]>  obtenerPersonData(String name);
+
+    @Query("select p.name, p.programmingLanguage from Person p where p.programmingLanguage=?1 and p.name=?2")
+    List<Object[]>  obtenerPersonData(String programmingLanguage, String name);
 }
